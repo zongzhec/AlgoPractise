f8653ec305d04dea403b938764258196898cfb14
zongzhec <zongzhe_chen@sina.com>
2020-06-08 19:25:48 +0800

    Enhance HashTable

--- src/main/java/foo/zongzhe/data_structure/line/hash_table/HashTableDemo.java
+++ src/main/java/foo/zongzhe/data_structure/line/hash_table/HashTableDemo.java
@@ -9,7 +9,10 @@ public class HashTableDemo {
         EmployeeSystem empSys = new EmployeeSystem();
 
         empSys.add(new Employee(1,"Zongzhe"));
-        empSys.add(new Employee(2,"Zongzhe"));
-        empSys.add(new Employee(1,"Zongzhe"));
+        empSys.add(new Employee(2,"Zongzhe2"));
+        empSys.add(new Employee(16,"Zongzhe16"));
+        empSys.add(new Employee(1,"ZongzheOther1"));
+
+        empSys.printAllEmp();
     }
 }
